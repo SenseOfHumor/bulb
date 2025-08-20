08d773bd6d8959919874e0547faedc0420f3f473
SWAPNIL <swa2314@gmail.com>
2025-08-20 17:49:15 -0400

    chore: update GitHub Actions workflow for deployment; refine build steps and add asset handling in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/bulb/',              
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            return `assets/images/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+      },
+    },
+  },
+  assetsInclude: ['**/*.svg'],
 })
